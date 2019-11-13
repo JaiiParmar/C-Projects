4a232477b95792782bdf5b23f109a63aa1c5915b
Jai Parmar <jaiparmar207@gmail.com>
2019-11-14 00:16:19 +0530

    REVERSE the list

--- STL/STL/LinkedListApp.cpp
+++ STL/STL/LinkedListApp.cpp
@@ -6,7 +6,15 @@ void main() {
 	std::forward_list<int> fl;
 	for (size_t i = 0; i < 10; i++)
 		fl.push_front(rand());
+	std::cout << std::endl << " ORIGINAL";
 	for (auto i = fl.begin(); i != fl.end(); i++) {
 		std::cout << " -- " << *i;
 	}
+
+	fl.reverse();
+	std::cout << std::endl << "REVERSE";
+	for (auto i = fl.begin(); i != fl.end(); i++) {
+		std::cout << " -- " << *i;
+	}
+
 }
