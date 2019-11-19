cf6ea8cd8a7499935b41a81421c55ac039587315
Jai Parmar <jaiparmar207@gmail.com>
2019-11-19 10:27:05 +0530

    Priority Queue Implemented:

--- STL/STL/deque.cpp
+++ STL/STL/deque.cpp
@@ -20,36 +20,36 @@ void FillUp(T& dq, int size) {
 	std::cout << std::endl << "FILLED UP" << std::endl << std::endl;
 }
 
-
-void main() {
-	
-	std::deque<int> dq(10);
-
-	FillUp(dq, 9);
-	showDeque(dq);
-	dq.push_back(11111);
-	dq.emplace_front(121212);
-	showDeque(dq);
-	std::cout << std::endl << "SIZE : " << dq.size();
-	dq.resize(5);	
-	showDeque(dq);
-	std::cout << std::endl << "SIZE : " << dq.size();
-
-	dq.resize(10);
-	showDeque(dq);
-	std::cout << std::endl << "SIZE : " << dq.size();
-
-	dq.push_back(1000000);
-	showDeque(dq);
-	std::cout << std::endl << "SIZE : " << dq.size();
-
-	dq.erase(dq.begin() + 5, dq.end() - 1);
-	showDeque(dq);
-	std::cout << std::endl << "SIZE : " << dq.size();
-
-	dq.insert(dq.cbegin() + 5, 4 ,9999999);
-	showDeque(dq);
-	std::cout << std::endl << "SIZE : " << dq.size();
-
-}
-
+//
+//int main() {
+//	
+//	std::deque<int> dq(10);
+//
+//	FillUp(dq, 9);
+//	showDeque(dq);
+//	dq.push_back(11111);
+//	dq.emplace_front(121212);
+//	showDeque(dq);
+//	std::cout << std::endl << "SIZE : " << dq.size();
+//	dq.resize(5);	
+//	showDeque(dq);
+//	std::cout << std::endl << "SIZE : " << dq.size();
+//
+//	dq.resize(10);
+//	showDeque(dq);
+//	std::cout << std::endl << "SIZE : " << dq.size();
+//
+//	dq.push_back(1000000);
+//	showDeque(dq);
+//	std::cout << std::endl << "SIZE : " << dq.size();
+//
+//	dq.erase(dq.begin() + 5, dq.end() - 1);
+//	showDeque(dq);
+//	std::cout << std::endl << "SIZE : " << dq.size();
+//
+//	dq.insert(dq.cbegin() + 5, 4 ,9999999);
+//	showDeque(dq);
+//	std::cout << std::endl << "SIZE : " << dq.size();
+//
+//}
+//
--- STL/STL/priority_queue.cpp
+++ STL/STL/priority_queue.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
 #include<queue>
+#include<string>
+#include<iomanip>
+#include<vector>
 
-//void main() {	
-//	std::priority_queue<int> pq;
-//
-//}
+using namespace std;
+
+template <typename T>
+void display(T& pq)
+{
+	cout << endl << endl << "START :";
+	while(!pq.empty()){
+		cout << setw(10) << pq.top();
+		pq.pop();
+	}
+}
+
+void main() {
+	
+	priority_queue<string, vector<string>, greater<string>> pq;
+	
+	pq.push(string("Manish"));
+	pq.push(string("Ajit"));
+	pq.push(string("Sanjai"));
+	pq.push(string("Priyanka"));
+	pq.push(string("Neha"));
+	display(pq);
+}
